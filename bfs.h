7d63e16cce51f31e5bf09a950b35559593c41d9c
HannahOnverwagt <33282044+HannahOnverwagt@users.noreply.github.com>
2020-11-30 15:33:41 +0100

    Update bfs.h
    Added seq_delta function

--- bfs.h
+++ bfs.h
@@ -8,6 +8,7 @@ extern long SPARSITY;
 
 long *bfs_linked(short **adjacency, long source);
 long *bfs_vec(short **adjacency, long source);
+long double *seq_delta(short **adjacency, long source, long distances);
 short **generate_symmetric_matrix();
 short **fill_buffer(short graph[NR_VERTICES][NR_VERTICES]);
 void free_variables(struct Node *first, struct Node *second, struct Node *third, struct Node *fourth);
